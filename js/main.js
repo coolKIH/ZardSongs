fb38a671c99aa1edb93bb3fb6a95425c9a8907f0
haoyellow <mehaoyellow@gmail.com>
2026-03-05 17:21:00 +0800

    feat: enhance UX and improve responsive design for lyrics page
    - UI: Implement 100vh immersive header with fade-in effect and auto-scroll.
    - UI: Fix search input to top for persistent access.
    - UX: Add "no results found" hint to improve search feedback.
    - Style: Update lyric cards to stack vertically (col-12) on mobile for better readability.
    - Refactor: Standardize indentation and spacing across index.html and main.js.
    - Fix: Prevent footer flickering at top by nesting it within Vue template.

--- js/main.js
+++ js/main.js
@@ -11,7 +11,8 @@
                 songLink: '',
                 playStatus: 'Loading..',
                 loadedDataHandler: null,
-                endedHandler: null
+                endedHandler: null,
+                headerImageLoaded: false
             }
         },
         computed: {
@@ -32,15 +33,52 @@
             }
         },
         mounted: function () {
+            var self = this;
+            var skipAnimation = false;
+
             this.fetchLrcData('zard_lrc.json');
+
+            var checkScroll = function () {
+                if (window.scrollY > 0) {
+                    skipAnimation = true;
+                }
+                window.removeEventListener('scroll', checkScroll);
+            };
+
+            window.addEventListener('scroll', checkScroll);
+
+            // Detect when header background image is loaded
+            var img = new Image();
+            img.onload = function () {
+                // Image loaded, trigger fade-in effect
+                self.headerImageLoaded = true;
+
+                // After image fade-in, scroll to welcome message
+                setTimeout(function () {
+                    if (skipAnimation) {
+                        return;
+                    }
+
+                    window.removeEventListener('scroll', checkScroll);
+
+                    var welcomeMsg = document.getElementById('welcomeMessage');
+                    if (welcomeMsg) {
+                        welcomeMsg.scrollIntoView({
+                            behavior: 'smooth',
+                            block: 'end'
+                        });
+                    }
+                }, 2500);
+            };
+            img.src = '../img/zard-01.jpg';
         },
         methods: {
             fetchLrcData: function (filename) {
                 var self = this;
-                this.$http.get('./data/'+filename)
+                this.$http.get('./data/' + filename)
                     .catch(function (err) {
                         // Fallback to alternative relative path
-                        return self.$http.get('data/'+filename);
+                        return self.$http.get('data/' + filename);
                     })
                     .then(function (lrcItems) {
                         return lrcItems.json();
@@ -53,7 +91,7 @@
             splitLines: function (str) {
                 var reg = /\r\n|\n|\r/;
                 var m = str.match(reg);
-                if (!m) return '<span>'+str+'</span>';
+                if (!m) return '<span>' + str + '</span>';
                 return '<span>' + str.slice(0, m.index) + '</span>' + this.splitLines(str.slice(m.index + m[0].length));
             },
             playMusic: function (songTitle, songLink) {
@@ -64,7 +102,7 @@
                 this.playStatus = 'Loading..';
                 var mp = this.$refs.musicPlayer;
                 mp.pause();
-                
+
                 // Remove old event listeners if they exist
                 if (this.loadedDataHandler) {
                     mp.removeEventListener('loadeddata', this.loadedDataHandler);
@@ -72,11 +110,11 @@
                 if (this.endedHandler) {
                     mp.removeEventListener('ended', this.endedHandler);
                 }
-                
+
                 mp.src = songLink;
                 this.playingMusic = true;
                 this.songTitle = songTitle;
-                
+
                 // Create new event handlers and store references
                 this.loadedDataHandler = function () {
                     me.playStatus = 'Enjoying..'
@@ -84,11 +122,11 @@
                 this.endedHandler = function () {
                     me.playingMusic = false;
                 };
-                
+
                 // Add event listeners
                 mp.addEventListener('loadeddata', this.loadedDataHandler);
                 mp.addEventListener('ended', this.endedHandler);
-                
+
                 mp.load();
                 mp.play();
             },
